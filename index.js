dc5c68ccfac8759a0eb7d04d066eb1ee7d1004d4
RahulBis <rahulsinghbisht125@gmail.com>
2024-07-19 15:49:54 +0530

    change in cors of index.js

--- index.js
+++ index.js
@@ -10,12 +10,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+
 const PORT = process.env.PORT || 5000;
 const DATABASE_URL = process.env.DATABASE_URL;
 
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true,
+  }));
+
+
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+
 
 // {
 //     origin: 'http://localhost:5173',
